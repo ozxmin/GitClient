f6d66d166d4accf5a1594007c096157948231cf7
ozxmin <ozxmin@me.com>
2018-02-23 15:17:25 -0600

    Initial Commit: Working Language API Fetch and basic table population

---
+++ Git Client/GitHubRepo.swift
@@ -0,0 +1,54 @@
+//
+//  GitHubRepo.swift
+//  Git Client
+//
+//  Created by Ozx Vax on 2/23/18.
+//  Copyright © 2018 Ozx Vax. All rights reserved.
+//
+
+import Foundation
+
+struct Repos: Codable {
+    let total_count: Int
+    let incomplete_results: Bool
+    let items: [Items]
+}
+
+
+struct Items: Codable {
+    let name: String
+    let stargazers_count: Int
+    let avatar_url: URL
+}
+
+//func fetchLanguageQuerry(language: String) {
+//    var repos: Repos
+//    DispatchQueue.global(qos: .userInitiated).async {
+//        let myGitHub = URL(string:"https://api.github.com/search/repositories?q=language:\(language)&page=1&per_page=10")
+//        guard let URLContents = try? Data(contentsOf: myGitHub!) else { return }
+//        let decoder = JSONDecoder()
+//        decoder.dateDecodingStrategy = .iso8601
+//        do {
+//            repos = try decoder.decode(Repos.self, from: URLContents)
+//        } catch DecodingError.typeMismatch(let type, let context) {
+//            print("error:=================== \(type)")
+//            print("context:================= \(context)")
+//        } catch {
+//            print("error:==================== \(error.localizedDescription)")
+//        }
+//        print("total_count: \(repos.total_count)")
+//        print("incomplete_results: \(repos.incomplete_results)")
+//        for each in repos.items {
+//            print("item: \(each)")
+//        }
+//        DispatchQueue.main.async {
+//            print("main queue================")
+//            let jsonObj = try? JSONSerialization.jsonObject(with: URLContents, options: [])
+//            print(jsonObj.debugDescription)
+//
+//            self?.tableView.reloadData()
+//
+//        }
+//    }
+//}
+
---
+++ Git Client/SearchResults.swift
@@ -0,0 +1,147 @@
+//
+//  SearchResults.swift
+//  Git Client
+//
+//  Created by Ozx Vax on 2/22/18.
+//  Copyright © 2018 Ozx Vax. All rights reserved.
+//
+
+import UIKit
+
+//https://api.github.com/user/repos?page=2&per_page=100
+
+class SearchResults: UITableViewController, UISearchBarDelegate {
+   
+    var repos: Repos?
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchBar.delegate = self
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        guard let query = searchBar.text  else { return }
+        if searchBar.text == "" { return }
+        
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            let myGitHub = URL(string:"https://api.github.com/search/repositories?q=language:\(query)&page=1&per_page=10")
+            guard let URLContents = try? Data(contentsOf: myGitHub!) else { return }
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            do {
+                self?.repos = try decoder.decode(Repos.self, from: URLContents)
+            } catch DecodingError.typeMismatch(let type, let context) {
+                print("error:=================== \(type)")
+                print("context:================= \(context)")
+            } catch {
+                print("error:==================== \(error.localizedDescription)")
+            }
+            print("total_count: \(String(describing: self?.repos?.total_count))")
+            print("incomplete_results: \(String(describing: self?.repos?.incomplete_results))")
+            for each in (self?.repos?.items)! {
+                print("item: \(String(describing: each))")
+            }
+            DispatchQueue.main.async {
+                print("main queue================")
+                let jsonObj = try? JSONSerialization.jsonObject(with: URLContents, options: [])
+                print(jsonObj.debugDescription)
+                
+                self?.tableView.reloadData()
+                
+            }
+        }
+    }
+    
+    
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int { return 1 }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+        cell.textLabel?.text = "Nombre: \(String(describing: repos!.items[indexPath.row].name))"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let repositrycount = repos?.items.count {
+            return repositrycount
+        }
+        return 0
+    }
+    
+//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//            repos.remove(at: indexPath.row)
+//            // Delete the row from the data source
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//        }
+//    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if let cell = sender as? UITableViewCell,
+//            let indexPath = tableView.indexPath(for: cell),
+//            let platoDetail = segue.destination as? PlatoDetailViewController {
+//            platoDetail.dataRow = indexPath.row
+//        }
+//    }
+
+    
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
